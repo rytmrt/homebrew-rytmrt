ad57a5eca413c512ba1da7c051a3570c63e2f99f
Ryota Morita <morita.r@dorasu.com>
2015-10-05 21:11:58 +0900

    first commit

---
+++ sshfavo.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+HOMEBREW_SSHFAVO_VERSION='0.1.0'
+class Sshfavo < Formula
+  homepage 'https://github.com/rytmrt/sshfavo'
+  url 'https://github.com/rytmrt/sshfavo.git', :tag => "v#{HOMEBREW_SSHFAVO_VERSION}"
+  version HOMEBREW_SSHFAVO_VERSION
+  head 'https://github.com/rytmrt/sshfavo.git', :branch => 'master'
+
+  depends_on 'go' => :build
+
+  def install
+    ENV['GOPATH'] = buildpath
+    system 'go', 'get', "github.com/BurntSushi/toml"
+    system 'go', 'build', '-o', 'sshfavo'
+    bin.install 'sshfavo'
+  end
+end
